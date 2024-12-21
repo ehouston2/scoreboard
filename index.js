27682e2bef9d436c007322513e5bee15151246ae
Elijah Houston <122054557+ehouston2@users.noreply.github.com>
2024-12-20 18:54:19 -0800

    Commit initial files

---
+++ index.js
@@ -0,0 +1,38 @@
+let homeText = document.getElementById("home-el");
+
+let home = 0
+
+function homeOne() {
+    home += 1
+    homeText.textContent = home
+}
+
+function homeTwo() {
+    home += 2
+    homeText.textContent = home
+}
+
+function homeThree() {
+    home += 3
+    homeText.textContent = home
+}
+
+
+let awayText = document.getElementById("away-el");
+
+let away = 0
+
+function awayOne() {
+    away += 1
+    awayText.textContent = away
+}
+
+function awayTwo() {
+    away += 2
+    awayText.textContent = away
+}
+
+function awayThree() {
+    away += 3
+    awayText.textContent = away
+}
